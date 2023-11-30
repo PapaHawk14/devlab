65197139ecbba8be3ee35ad53e20ce12787d8bef
Grant Hawksworth <grant.hawksworth@gmail.com>
2023-11-30 12:46:22 +1030

    ansible-updates

--- ansible/aptproxmox.sh
+++ ansible/aptproxmox.sh
@@ -3,5 +3,5 @@
 echo -e " \033[33;2m   APT Update all   \033[0m"
 echo -e " \033[33;2m   including raspberry pis.   \033[0m"
 
-ansible-playbook ./playbook/apt.yml -i ./inv/hosts.ini --extra-vars "ansible_sudo_pass=GA!14cjezv"
+ansible-playbook ./playbook/aptproxmox.yml -i ./inv/hosts.ini --extra-vars "ansible_sudo_pass=GA!14cjezv"
 
